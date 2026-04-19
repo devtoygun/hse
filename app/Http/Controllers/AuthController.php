53b6326d0551eed4b3a253d7feab130b12787e1f
Alihan Toygun <dev.toygun@gmail.com>
2026-04-19 16:12:08 +0300

    auth launched

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(){
+        return view('layout.auth.login');
+    }
+    public function reset_password(){
+        return view('layout.auth.reset_password');
+    }
+    public function screen_lock(){
+        return view('layout.auth.screen_lock');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AppController;
+use App\Http\Controllers\AuthController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +16,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function(){
+   return redirect('/app');
+});
+
+Route::controller(AuthController::class)->prefix('auth')->group(function(){
+    Route::get('/login', 'login');
+    Route::get('/reset-password', 'reset_password');
+    Route::get('/screen-lock', 'screen_lock');
+
+    Route::post('/login', 'confirm_login');
+    Route::post('/reset-password', 'confirm_reset_password');
+    Route::post('/screen-lock', 'confirm_screen_lock');
+});
+
+Route::controller(AppController::class)->prefix('app')->middleware('auth')->group(function(){
+    Route::get('/', 'app');
 });
