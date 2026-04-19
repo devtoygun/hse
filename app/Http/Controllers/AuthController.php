f4ebab4206fd271cbc3984960a6a8347e712fd3f
Alihan Toygun <dev.toygun@gmail.com>
2026-04-19 16:23:21 +0300

    included theme assets

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,9 +10,9 @@ class AuthController extends Controller
         return view('layout.auth.login');
     }
     public function reset_password(){
-        return view('layout.auth.reset_password');
+        return view('layout.auth.reset-password');
     }
     public function screen_lock(){
-        return view('layout.auth.screen_lock');
+        return view('layout.auth.screen-lock');
     }
 }
