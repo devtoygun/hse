151fbe53a067b8582d20e9c675b2cb74646afb4d
Alihan Toygun <dev.toygun@gmail.com>
2026-04-23 21:21:37 +0300

    Simplify user table dom layout

--- resources/views/app/user/list-user.blade.php
+++ resources/views/app/user/list-user.blade.php
@@ -5,13 +5,54 @@
 @section('content')
     <div class="row g-4">
         <div class="col-12">
-            <div class="card">
-                <div class="card-body">
-                    <h4 class="card-title mb-2">Tum Kullanicilar</h4>
-                    <p class="text-muted mb-0">Sistemdeki standart kullanicilar bu ekranda listelenecek. Admin ve user rolleri ayri listelerde yonetilecek sekilde temel yapi hazirlandi.</p>
+            <!-- Ajax Sourced Server-side -->
+             <div class="card border">
+                <div class="card-header">
+                    <h4>Kullanıcı Listesi</h4>
                 </div>
-            </div>
+                <div class="card-body">
+                     <table class="userListTable table">
+                    <thead>
+                      <tr>
+                        <th>İsim</th>
+                        <th>E-posta</th>
+                        <th>Telefon</th>
+                        <th>Durum</th>
+                        <th>İlk Giriş</th>
+                        <th>Son Giriş</th>
+                        <th>İşlem</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($users as $user)
+                            <tr>
+                                <td>{{$user->firstname}}</td>
+                                <td>{{$user->firstname}}</td>
+                                <td>{{$user->firstname}}</td>
+                                <td>{{$user->firstname}}</td>
+                                <td>{{$user->firstname}}</td>
+                                <td>{{$user->firstname}}</td>
+                                <td>{{$user->firstname}}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                  </table>
+                </div>
+             </div>
         </div>
     </div>
 @endsection
 
+@section('script')
+    <script>
+        // 2. Şimdi senin bildiğin o düz mantıkla başlat
+    $('.userListTable').DataTable({
+        responsive: true,
+        processing: true,
+        language: {
+            url: '//cdn.datatables.net/plug-ins/1.13.4/i18n/tr.json' // Tabloyu Türkçe yapalım
+        },
+       dom: '<"row"<"col-sm-12 col-md-6"l><"col-sm-12 col-md-6 d-flex justify-content-center justify-content-md-end"f>>t<"row"<"col-sm-12 col-md-6"i><"col-sm-12 col-md-6"p>>',
+    });
+    </script>
+@endsection
