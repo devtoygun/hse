9f62a3f8176397a98bbb7d2f89d804fa35a8ecca
Alihan Toygun <dev.toygun@gmail.com>
2026-05-05 20:29:14 +0300

    refactor: make form question relation explicit

--- app/Models/FormQuestion.php
+++ app/Models/FormQuestion.php
@@ -43,6 +43,6 @@ class FormQuestion extends Model
      */
     public function form(): BelongsTo
     {
-        return $this->belongsTo(Form::class);
+        return $this->belongsTo(Form::class, 'form_id', 'id');
     }
 }
