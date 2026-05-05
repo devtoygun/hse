3265aecf5841a0d078688eaa61b69bd16f83f676
Alihan Toygun <dev.toygun@gmail.com>
2026-05-05 20:07:58 +0300

    feat: add user forms relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -53,5 +54,13 @@ class User extends Authenticatable
             'first_login' => 'integer',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Gets the forms created by the user.
+     */
+    public function forms(): HasMany
+    {
+        return $this->hasMany(Form::class, 'created_by');
     }
 }
